f515e92200a902126b89105ca2376ee2c791804b
Mavi <mariapimenta2607@gmail.com>
2024-10-09 11:06:37 -0300

    fix: req axios page

--- src/app/axios/page.tsx
+++ src/app/axios/page.tsx
@@ -1,7 +1,7 @@
 "use client"
-import axios from "axios";
 import Card from "../../components/card";
-import { useEffect, useState } from "react"; 
+import { Suspense, useEffect, useState } from "react"; 
+import { api } from "../../constant/api";
 
 interface IData {
   id: string;
@@ -14,8 +14,11 @@ export default function HomeAxios() {
 
   useEffect(() => {
     const load = async () => {
-      const res = await axios.get("https://rickandmortyapi.com/api/character");
-      setCharacters(res.data.results);
+      api.get("/character").then((res) => {
+        setCharacters(res.data.results);
+      }).catch((err) => {
+        console.log(err);
+      })
     }
     load();
   }, [])
@@ -23,14 +26,16 @@ export default function HomeAxios() {
   return (
     <>
       <div className="p-4 flex flex-wrap gap-6 justify-center align-center">
-      {characters.map((character: IData) => (
-        <Card 
-          key={character.id} 
-          image={character.image} 
-          name={character.name} 
-          id={character.id} 
-        />
-      ))}
+      <Suspense fallback={<div>Loading...</div>}>
+        {characters.map((character: IData) => (
+          <Card 
+            key={character.id} 
+            image={character.image} 
+            name={character.name} 
+            id={character.id} 
+          />
+        ))}
+      </Suspense>
       </div>
     </>
   );
